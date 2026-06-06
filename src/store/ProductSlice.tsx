5b48af1988e663bfabe74119d4b72cdf680482ec
genius4090 <abdulboriytolqinov00200@gmail.com>
2026-06-06 18:32:20 +0500

    fixed with npm run build

--- src/store/ProductSlice.tsx
+++ src/store/ProductSlice.tsx
@@ -1,33 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { Product } from "../@types";
 
-
-
 interface ProductState {
-    items: Product[]
-  }
-  
+  items: Product[]
+}
 
-  const initialState: ProductState = {
-    items: [
-      {
-        id: 1,
-        title: "flower",
-        price: 14,
-        stock: 5
-      },
-      {
-        id: 2,
-        title: "cactus",
-        price: 99,
-        stock: 10
-      }
-    ]
-  }
+const initialState: ProductState = {
+  items: [
+    { id: 1, title: "flower",  price: 14, stock: 5,  image_url: null },
+    { id: 2, title: "cactus",  price: 99, stock: 10, image_url: null },
+  ]
+}
+
 export const ProductSlice = createSlice({
-    name: "CartSlice",
-    initialState,
-    reducers: {}
+  name: "CartSlice",
+  initialState,
+  reducers: {}
 })
 
 export default ProductSlice.reducer

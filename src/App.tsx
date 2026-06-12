009c16524f07f076294c781ddd8464189036c1f2
genius4090 <abdulboriytolqinov00200@gmail.com>
2026-06-12 14:52:09 +0500

    last fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import DashboardLayout from "./layouts/DashboardLayout";
 import Home from "./pages/Home";
